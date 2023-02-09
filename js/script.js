3d30556ae4f014d297f43d3e64eb3102f55d8f77
Sanjeet Mistry <sanjeetmistry10@gmail.com>
2023-02-09 22:47:06 +0530

    Add Archives with JS

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let archives = [
+  {name: "March 2014", url: ""},
+  {name: "February 2014", url: ""},
+  {name: "January 2014", url: ""},
+  {name: "December 2013", url: ""},
+  {name: "November 2013", url: ""},
+  {name: "October 2013", url: ""},
+  {name: "September 2013", url: ""},
+  {name: "August 2013", url: ""},
+  {name: "July 2013", url: ""},
+  {name: "June 2013", url: ""},
+  {name: "May 2013", url: ""},
+  {name: "April 2013", url: ""}
+];
+
+const body = document.body;
+const archivesDiv = body.querySelector(".blogs-section aside .archives");
+
+const addArchives = () => {
+  const ul = document.createElement("ul");
+  archivesDiv.append(ul);
+  for(let i = 0; i < archives.length; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", "#FIXME");
+    a.setAttribute("title", archives[i].name);
+    a.innerText = archives[i].name;
+
+    li.append(a);
+    ul.append(li);
+  }
+}
+
+addArchives();
